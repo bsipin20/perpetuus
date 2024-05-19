bb0f1937ff4af7033c289a05e2606e5a8627efc2
brian <bsipin@gmail.com>
2024-05-18 21:21:30 -0400

    stuff

--- go/hello/hello.go
+++ go/hello/hello.go
@@ -2,9 +2,18 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+
+
 func Hello(name string) string {
 	if name == "" {
       return "Hello, World!"
+  }
+  if language == "spanish" {
+    return spanishHelloPrefix + name
+
   }
 	return "Hello, " + name + "!"
 }
